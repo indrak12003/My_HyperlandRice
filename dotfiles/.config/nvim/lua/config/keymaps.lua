a380c279392905f873f55085561c8e6092998f12
amnesja <bagganaman43@gmail.com>
2026-01-12 23:57:11 +0100

    sistemazione

--- dotfiles/.config/nvim/lua/config/keymaps.lua
+++ dotfiles/.config/nvim/lua/config/keymaps.lua
@@ -1,16 +1,34 @@
--- Leader
+-- LEADER
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- Keymaps
-vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "Quit" })
-vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save" })
+-- Opzioni comuni per tutte le mappature
+local opts = { noremap = true, silent = true }
 
+local keymap = vim.keymap.set
+
+-----------------------------------------
+-- File / Buffer
+-----------------------------------------
+keymap("n", "<leader>q", ":q<CR>", { desc = "Quit", unpack(opts) })
+keymap("n", "<leader>w", ":w<CR>", { desc = "Save", unpack(opts) })
+
+-----------------------------------------
 -- Telescope
-vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
-vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
-vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
-vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") 
+-----------------------------------------
+keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find Files", unpack(opts) })
+keymap("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Live Grep", unpack(opts) })
+keymap("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Buffers", unpack(opts) })
+keymap("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Help Tags", unpack(opts) })
 
+-----------------------------------------
 -- Neo-tree
-vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>")
+-----------------------------------------
+keymap("n", "<leader>e", ":Neotree toggle<CR>", { desc = "Toggle File Explorer", unpack(opts) })
+
+-----------------------------------------
+-- Null-ls
+-----------------------------------------
+keymap("n", "<leader>f", function()
+  vim.lsp.buf.format({ async = true })
+end, { desc = "Format file", unpack(opts) })
