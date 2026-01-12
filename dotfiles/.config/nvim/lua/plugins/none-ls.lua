af51ea9cb82818d0dc9555e3800b844956003feb
amnesja <bagganaman43@gmail.com>
2026-01-12 23:52:06 +0100

    none-ls

--- dotfiles/.config/nvim/lua/plugins/none-ls.lua
+++ dotfiles/.config/nvim/lua/plugins/none-ls.lua
@@ -1,26 +1,36 @@
 return {
   "nvimtools/none-ls.nvim",
-  dependencies = { "williamboman/mason.nvim", "jay-babu/mason-null-ls.nvim" },
+  dependencies = {
+    "williamboman/mason.nvim",
+    "jay-babu/mason-null-ls.nvim",
+  },
 
   config = function()
     require("mason-null-ls").setup({
       ensure_installed = {
         "black",
         "ruff",
-        "prettier",
         "stylua",
+        "prettier",
       },
-      automatic_setup = true,
+      automatic_installation = true,
     })
 
     local null_ls = require("null-ls")
 
     null_ls.setup({
       sources = {
+        -- Formatter Python
         null_ls.builtins.formatting.black,
+
+        -- Ruff come formatter (non diagnostico)
+        null_ls.builtins.formatting.ruff,
+
+        -- Lua formatter
         null_ls.builtins.formatting.stylua,
+
+        -- Web formatter
         null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.ruff,
       },
     })
   end,
